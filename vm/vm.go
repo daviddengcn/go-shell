1239760493b714ca2d6d39fd4e9810fc521642dd
David Deng <daviddengcn@gmail.com>
2015-03-04 10:03:01 -0800

    Return multi value in evalExpr

--- vm/vm.go
+++ vm/vm.go
@@ -89,74 +89,92 @@ func (mch *machine) findSelected(ns NameSpace, x, sel string) (reflect.Value, er
 	return noValue, fmt.Errorf("Undefined: %s", x)
 }
 
-// Returns the value itself not the pointer.
-func (mch *machine) evalExpr(ns NameSpace, expr ast.Expr) (reflect.Value, error) {
+func checkSingleValue(vls []reflect.Value, err error) (reflect.Value, error) {
+	if err !=  nil {
+		return noValue, err
+	}
+	if len(vls) != 1 {
+		return noValue, fmt.Errorf("multiple-value(%d) in single-value context", len(vls))
+	}
+	return vls[0], nil
+}
+
+func fromSingleValue(vl reflect.Value, err error) ([]reflect.Value, error) {
+	if err != nil {
+		return nil, err
+	}
+	return []reflect.Value{vl}, nil
+}
+
+// Returns slice of values themselves not the pointers.
+func (mch *machine) evalExpr(ns NameSpace, expr ast.Expr) ([]reflect.Value, error) {
 	switch expr := expr.(type) {
 	case *ast.BasicLit:
 		switch expr.Kind {
 		case token.INT:
 			i, _ := strconv.Atoi(expr.Value)
-			return reflect.ValueOf(i), nil
+			return []reflect.Value{reflect.ValueOf(i)}, nil
 
 		case token.STRING:
 			s, _ := strconv.Unquote(expr.Value)
-			return reflect.ValueOf(s), nil
+			return []reflect.Value{reflect.ValueOf(s)}, nil
 
 		default:
 			log.Println("Unknown BasicLit Kind")
 			ast.Print(token.NewFileSet(), expr)
-			return reflect.ValueOf(expr), nil
+			return []reflect.Value{reflect.ValueOf(expr)}, nil
 		}
 
 	case *ast.Ident:
 		v := ns.FindVar(expr.Name)
 		if v == noValue {
-			return noValue, fmt.Errorf("Unknown Ident %v", expr.Name)
+			return nil, fmt.Errorf("Unknown Ident %v", expr.Name)
 		}
-		return reflect.Indirect(v), nil
+		return []reflect.Value{reflect.Indirect(v)}, nil
 
 	case *ast.CallExpr:
-		fn, err := mch.evalExpr(ns, expr.Fun)
+		fn, err := checkSingleValue(mch.evalExpr(ns, expr.Fun))
 		if err != nil {
-			return noValue, err
+			return nil, err
 		}
 
 		if fn.Kind() != reflect.Func {
-			return noValue, fmt.Errorf("cannot call non-function (type %s)", fn.Type())
+			return nil, fmt.Errorf("cannot call non-function (type %s)", fn.Type())
 		}
 
+		// TODO when len(expr.Args) == 1, check multi return value situation
 		args := make([]reflect.Value, len(expr.Args))
 		for i, arg := range expr.Args {
-			argV, err := mch.evalExpr(ns, arg)
+			argV, err := checkSingleValue(mch.evalExpr(ns, arg))
 			if err != nil {
-				return noValue, err
+				return nil, err
 			}
 			args[i] = argV
 		}
 
-		results := fn.Call(args)
-		return reflect.ValueOf(results), nil
+		return fn.Call(args), nil
 
 	case *ast.SelectorExpr:
 		switch x := expr.X.(type) {
 		case *ast.Ident:
-			return mch.findSelected(ns, x.Name, expr.Sel.Name)
+			return fromSingleValue(mch.findSelected(ns, x.Name, expr.Sel.Name))
 		default:
 			log.Println("Unknown SelectorExpr X type")
 			ast.Print(token.NewFileSet(), x)
-			return reflect.ValueOf(expr), nil
+			return []reflect.Value{reflect.ValueOf(expr)}, nil
 		}
 
 	default:
 		log.Println("Unknown expr type")
 		ast.Print(token.NewFileSet(), expr)
-		return reflect.ValueOf(expr), nil
+		return []reflect.Value{reflect.ValueOf(expr)}, nil
 	}
 }
 
 func (mch *machine) runStatement(ns NameSpace, st ast.Stmt) error {
 	switch st := st.(type) {
 	case *ast.AssignStmt:
+		// TODO when len(st.Rhs) == 1, check multi value return
 		if len(st.Lhs) != len(st.Rhs) {
 			return fmt.Errorf("assignment count mismatch: %d %s %d", len(st.Lhs), st.Tok, len(st.Rhs))
 		}
@@ -176,7 +194,7 @@ func (mch *machine) runStatement(ns NameSpace, st ast.Stmt) error {
 				lIdent := l.(*ast.Ident)
 				r := st.Rhs[i]
 				if v := ns.FindLocalVar(lIdent.Name); v == noValue {
-					vl, err := mch.evalExpr(ns, r)
+					vl, err := checkSingleValue(mch.evalExpr(ns, r))
 					if err != nil {
 						return err
 					}
@@ -187,7 +205,7 @@ func (mch *machine) runStatement(ns NameSpace, st ast.Stmt) error {
 			}
 		} else {
 			for i, l := range st.Lhs {
-				lV, err := mch.evalExpr(ns, l)
+				lV, err := checkSingleValue(mch.evalExpr(ns, l))
 				if err != nil {
 					return err
 				}
@@ -200,17 +218,18 @@ func (mch *machine) runStatement(ns NameSpace, st ast.Stmt) error {
 					return err
 				}
 
-				lV.Set(rV)
+				lV.Set(rV[0])
 			}
 		}
 
 	case *ast.ExprStmt:
 		_, err := mch.evalExpr(ns, st.X)
 		return err
-
+		
 	default:
 		log.Println("Unknown statement type")
 		ast.Print(token.NewFileSet(), st)
+		return fmt.Errorf("Unknown statement type")
 	}
 	return nil
 }
@@ -228,7 +247,6 @@ const (
 )
 
 func (mch *machine) Run(line string) (isFragment bool) {
-
 	src := srcPrefix + line + srcSuffix
 
 	nLines := len(strings.Split(src, "\n"))
